bcbc7fd9cd5abfdf590a6cbfb515c1ae3dac254d
anja <anja.tonsa@elfak.rs>
2023-06-24 23:40:45 +0200

    Starting with Reddit API

---
+++ TreciProjekat/Program.cs
@@ -0,0 +1,47 @@
+﻿using Reddit;
+using Reddit.Controllers;
+using System;
+namespace TreciProjekat
+{
+    internal class Program
+    {
+        static string clientId = Environment.GetEnvironmentVariable("REDDIT_API_CLIENT_ID");
+        static string secret= Environment.GetEnvironmentVariable("REDDIT_API_SECRET");
+        //static string accessToken3 = Environment.GetEnvironmentVariable("REDDIT_API_TOKEN");
+
+
+        static async Task Main()
+        {
+            RedditAPI r = new RedditAPI();
+            string accessToken = await r.getAccessToken(clientId, secret);
+            RedditClient reddit = new RedditClient(accessToken: accessToken);
+
+
+            Subreddit subreddit = reddit.Subreddit("AskAcademia").About();
+            Console.WriteLine($"Subreddit Name: {subreddit.Description}");
+
+
+            List<Post> posts = subreddit.Posts.GetTop("all", "10"); // Replace "10" with the number of posts you want to retrieve
+
+            foreach (Post post in posts)
+            {
+                List<Comment> comments = post.Comments.GetComments();
+
+                foreach (Comment comment in comments)
+                {
+                    // Process each comment as needed
+                    Console.WriteLine(comment.Body);
+                }
+            }
+
+        }
+
+
+
+    }
+
+
+
+}
+
+
---
+++ TreciProjekat/RedditAPI.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreciProjekat
+{
+    public class RedditAPI
+    {
+        public async Task<string> getAccessToken(string clientId, string secret)
+        {
+            var client = new HttpClient();
+
+            var tokenRequestBody = new Dictionary<string, string>
+            {
+                { "grant_type", "client_credentials" },
+            };
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
+                "Basic", Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{clientId}:{secret}")));
+            client.DefaultRequestHeaders.Add("User-Agent", "Getting the access token for the project.");
+
+            var tokenResponse = await client.PostAsync("https://www.reddit.com/api/v1/access_token", new FormUrlEncodedContent(tokenRequestBody));
+            var responseContent = await tokenResponse.Content.ReadAsStringAsync();
+
+            var jsonResponse=JObject.Parse(responseContent);
+            var accessToken = jsonResponse["access_token"]?.ToString();
+            return accessToken;
+        }
+
+    }
+}
